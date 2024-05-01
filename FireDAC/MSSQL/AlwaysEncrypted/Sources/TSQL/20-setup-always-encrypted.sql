b937e2e92edecf9cfde6d045b1370aac3f2df73e
Sergio Govoni <segovoni@gmail.com>
2024-05-01 15:42:39 +0200

    Added SQL Server Always Encrypted configuration script

---
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/20-setup-always-encrypted.sql
@@ -0,0 +1,16 @@
+------------------------------------------------------------------------
+-- Event:        Delphi Day 2024 - June 11-12                         --
+--               https://www.delphiday.it/                            --
+--                                                                    --
+-- Session:      SQL Server Encryption: Data protection in a          --
+--               Delphi Applications!                                 --
+--                                                                    --
+-- Demo:         Setup Always Encrypted                               --
+-- Author:       Sergio Govoni                                        --
+-- Notes:        --                                                   --
+------------------------------------------------------------------------
+
+USE [master];
+GO
+
+-- Links
