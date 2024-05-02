80650c290fdc5e94e8f35e27af45ab6478b645bd
Sergio Govoni <segovoni@gmail.com>
2024-05-02 22:49:58 +0200

    Added Always Encrypted documentation links

--- FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/20-setup-always-encrypted.sql
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/20-setup-always-encrypted.sql
@@ -5,7 +5,7 @@
 -- Session:      SQL Server Encryption: Data protection in a          --
 --               Delphi Applications!                                 --
 --                                                                    --
--- Demo:         Setup Always Encrypted                               --
+-- Demo:         Setup SQL Server Always Encrypted                    --
 -- Author:       Sergio Govoni                                        --
 -- Notes:        --                                                   --
 ------------------------------------------------------------------------
@@ -13,4 +13,21 @@
 USE [master];
 GO
 
--- Links
+-- Always Encrypted documentation
+-- https://learn.microsoft.com/azure/azure-sql/database/always-encrypted-landing 
+
+
+-- Analyze the impacts due to the possible change of COLLATE for the encrypted columns
+-- https://learn.microsoft.com/sql/relational-databases/collations/collation-and-unicode-support
+
+
+-- Analyze possible impacts on client applications (parametric queries, connection strings, supported drivers)
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-client-development
+
+
+-- How queries against encrypted columns work
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-database-engine#how-queries-against-encrypted-columns-work
+
+
+-- Analyze existing Limitations
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-database-engine#limitations
