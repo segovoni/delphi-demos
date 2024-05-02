e38d6408b723c947b2ee6824c8334730a4b1ae69
Sergio Govoni <segovoni@gmail.com>
2024-05-02 22:52:22 +0200

    Added demo step descriptions

--- FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/30-working-with-encrypted-data.sql
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/30-working-with-encrypted-data.sql
@@ -14,15 +14,64 @@ USE [AlwaysEncryptedDB];
 GO
 
 
--- INSERTs into a table with encrypted columns
+-- Enable Always Encrypted (column encryption) set to disabled
+-- Query the dbo.Person
 
--- It fails
+-- SSMS will not be able to decrypt the data stored in the encrypted columns;
+-- the following query will return the encrypted data
+SELECT * FROM dbo.Person;
+GO
+
+
+-- Enable Always Encrypted (column encryption) set to enabled
+-- SSMS will attempt to decrypt the data stored in the encrypted columns
+-- using the previously created cryptographic keys
+SELECT * FROM dbo.Person;
+GO
+
+
+-- Let’s try to insert a new record in the dbo.Person
+
+-- This query will fail
 INSERT INTO dbo.Person
   (FirstName, LastName, SocialSecurityNumber, CreditCardNumber, Salary)
 VALUES
   ('Janice', 'Galvin', '327-89-2514', '9999-1111-2222-3333', $38115);
 GO
 
+
+-- Operand type clash: varchar is incompatible with varchar(8000)
+-- encrypted with (encryption_type = 'DETERMINISTIC', 
+-- encryption_algorithm_name = 'AEAD_AES_256_CBC_HMAC_SHA_256', 
+-- column_encryption_key_name = 'CEK_Auto1', 
+-- column_encryption_key_database_name = 'AlwaysEncryptedDB') 
+-- collation_name = 'SQL_Latin1_General_CP1_CI_AS'
+
+-- When a query tries to insert data into encrypted columns or when it attempts 
+-- to filter data based on one or more encrypted columns, passing values or
+-- T-SQL variables corresponding to the encrypted columns is not supported
+
+-- Inserting values into encrypted columns is allowed only using parameters
+
+DECLARE
+  @SocialSecurityNumber CHAR(11) = '327-89-2514'
+  ,@CreditCardNumber CHAR(19) = '9999-1111-2222-3333'
+  ,@Salary MONEY = $38115;
+
+INSERT INTO dbo.Person
+  (FirstName, LastName, SocialSecurityNumber, CreditCardNumber, Salary)
+VALUES
+  ('Janice', 'Galvin', @SocialSecurityNumber, @CreditCardNumber, @Salary);
+GO
+
+
+-- Encryption scheme mismatch for columns/variables '@SocialSecurityNumber'.
+-- The encryption scheme for the columns/variables is (encryption_type = 'PLAINTEXT')
+-- and the expression near line '6' expects it to be DETERMINISTIC, or PLAINTEXT
+
+
+-- Enabling parameterization for Always Encrypted
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-query-columns-ssms#enabling-and-disabling-parameterization-for-always-encrypted
 
 
 DECLARE
@@ -47,6 +96,7 @@ SET
   Salary = @Salary
 WHERE
   FirstName = 'Janice';
+GO
 
 
 DECLARE
@@ -58,5 +108,15 @@ SET
   SocialSecurityNumber = @SocialSecurityNumber
 WHERE
   FirstName = 'Janice';
+GO
 
-DECLARE @NonEncryptedValue CHAR(11) = '795-00-0000'; UPDATE dbo.Person SET SocialSecurityNumber = @NonEncryptedValue
+
+/*
+DECLARE @NonEncryptedValue CHAR(11) = '795-00-0000';
+
+UPDATE
+  dbo.Person
+SET
+  SocialSecurityNumber = @NonEncryptedValue;
+GO
+*/
