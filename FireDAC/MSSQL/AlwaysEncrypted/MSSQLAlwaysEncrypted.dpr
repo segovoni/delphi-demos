1dae6c9686250889389dae6a4a332751f335f195
Sergio Govoni <segovoni@gmail.com>
2024-05-14 08:51:38 +0200

    Added new units to the project

--- FireDAC/MSSQL/AlwaysEncrypted/MSSQLAlwaysEncrypted.dpr
+++ FireDAC/MSSQL/AlwaysEncrypted/MSSQLAlwaysEncrypted.dpr
@@ -5,14 +5,18 @@ uses
   MSSQLAlwaysEncrypted.FMain in 'Sources\Views\MSSQLAlwaysEncrypted.FMain.pas' {frmAlwaysEncryptedMain},
   MSSQLAlwaysEncrypted.Interfaces in 'Sources\Interfaces\MSSQLAlwaysEncrypted.Interfaces.pas',
   MSSQLAlwaysEncrypted.DataModule in 'Sources\DataModules\MSSQLAlwaysEncrypted.DataModule.pas' {DM: TDataModule},
-  MSSQLAlwaysEncrypted.MainPresenter in 'Sources\Presenters\MSSQLAlwaysEncrypted.MainPresenter.pas';
+  MSSQLAlwaysEncrypted.MainPresenter in 'Sources\Presenters\MSSQLAlwaysEncrypted.MainPresenter.pas',
+  MSSQLAlwaysEncrypted.FMainSimplified in 'Sources\Views\MSSQLAlwaysEncrypted.FMainSimplified.pas' {frmAlwaysEncryptedMainSimplified},
+  MSSQLAlwaysEncrypted.MainSimplifiedPresenter in 'Sources\Presenters\MSSQLAlwaysEncrypted.MainSimplifiedPresenter.pas',
+  MSSQLAlwaysEncrypted.Base.ActiveRecord in 'Sources\Models\MSSQLAlwaysEncrypted.Base.ActiveRecord.pas',
+  MSSQLAlwaysEncrypted.Person.ActiveRecord in 'Sources\Models\MSSQLAlwaysEncrypted.Person.ActiveRecord.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TfrmAlwaysEncryptedMain, frmAlwaysEncryptedMain);
+  Application.CreateForm(TfrmAlwaysEncryptedMainSimplified, frmAlwaysEncryptedMainSimplified);
   Application.CreateForm(TDM, DM);
   Application.Run;
 end.
