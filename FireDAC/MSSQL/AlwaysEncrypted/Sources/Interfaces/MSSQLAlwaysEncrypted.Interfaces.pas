7bfd29dc4551de1e21fdd11a23a5a4f4c1391497
Sergio Govoni <segovoni@gmail.com>
2024-05-14 08:34:35 +0200

    Added new interface for new main simplified view

--- FireDAC/MSSQL/AlwaysEncrypted/Sources/Interfaces/MSSQLAlwaysEncrypted.Interfaces.pas
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/Interfaces/MSSQLAlwaysEncrypted.Interfaces.pas
@@ -8,7 +8,7 @@ uses
 type
   IMainView = interface
     ['{60C0203E-A043-4751-B2E7-6E7CEC418C43}']
-    // input (function)
+    // Input (function)
     function GetDriverID: string;
     function GetDataSource: string;
     function GetServerName: string;
@@ -22,13 +22,43 @@ type
     function GetNonEncryptedValue: string;
     function GetColumnNameWhere: string;
     function GetColumnValueWhere: string;
-    // output (procedure)
+    // Output (procedure)
     procedure Connect;
     procedure OpenQuery;
     procedure ExecUpdate;
     procedure DisplayMessage(AValue: string);
-    // input/output (function with params)
+    // Input/Output (function with params)
     function AskConfirmation(AValue: string): Boolean;
+  end;
+
+  IMainSimplifiedView = interface
+    ['{B7A593AC-A9D2-4E28-9260-4B00D097E3E3}']
+    // Input (function)
+    function GetDriverID: string;
+    function GetServerName: string;
+    function GetDatabaseName: string;
+    function GetUserName: string;
+    function GetPassword: string;
+    function GetTrustServerCertificate: Boolean;
+    function GetColumnEncryption: Boolean;
+    function GetSELECTSQLText: string;
+    function GetdsQueryEncryptedData: TDataSource;
+    function GetFirstName: string;
+    function GetLastName: string;
+    function GetSocialSecurityNumber: string;
+    function GetCreditCardNumber: string;
+    function GetSalary: Currency;
+    // Output (procedure)
+    procedure Connect;
+    procedure OpenQuery;
+    procedure Update;
+    procedure DisplayPerson;
+    procedure DisplayFirstName(AValue: string);
+    procedure DisplayLastName(AValue: string);
+    procedure DisplaySocialSecurityNumber(AValue: string);
+    procedure DisplayCreditCardNumber(AValue: string);
+    procedure DisplaySalary(AValue: Currency);
+    procedure DisplayMessage(AValue: string);
   end;
 
 implementation
