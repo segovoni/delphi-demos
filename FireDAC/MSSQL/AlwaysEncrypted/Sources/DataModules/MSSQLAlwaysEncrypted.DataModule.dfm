b2bab945dfd8f0f9c67d50b79a1e909f4ecc1173
Sergio Govoni <segovoni@gmail.com>
2024-05-14 08:32:58 +0200

    Added new FireDAC component FDPhysMSSQLDriverLink

--- FireDAC/MSSQL/AlwaysEncrypted/Sources/DataModules/MSSQLAlwaysEncrypted.DataModule.dfm
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/DataModules/MSSQLAlwaysEncrypted.DataModule.dfm
@@ -1,20 +1,24 @@
 object DM: TDM
   OldCreateOrder = False
-  Height = 205
-  Width = 263
+  Height = 266
+  Width = 349
   object FDConnection: TFDConnection
-    Left = 56
-    Top = 40
+    Left = 64
+    Top = 16
   end
   object FDQrySelectEncryptedData: TFDQuery
     AutoCalcFields = False
     Connection = FDConnection
-    Left = 120
-    Top = 104
+    Left = 248
+    Top = 16
   end
   object FDQryUpdateEncryptedData: TFDQuery
     Connection = FDConnection
-    Left = 160
-    Top = 48
+    Left = 64
+    Top = 80
+  end
+  object FDPhysMSSQLDriverLink: TFDPhysMSSQLDriverLink
+    Left = 64
+    Top = 152
   end
 end
--- FireDAC/MSSQL/AlwaysEncrypted/Sources/DataModules/MSSQLAlwaysEncrypted.DataModule.pas
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/DataModules/MSSQLAlwaysEncrypted.DataModule.pas
@@ -7,13 +7,15 @@ uses
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
   Data.DB, FireDAC.Comp.Client, FireDAC.Stan.Param, FireDAC.DatS,
-  FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet;
+  FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet, FireDAC.Phys.MSSQLDef,
+  FireDAC.Phys.ODBCBase, FireDAC.Phys.MSSQL;
 
 type
   TDM = class(TDataModule)
     FDConnection: TFDConnection;
     FDQrySelectEncryptedData: TFDQuery;
     FDQryUpdateEncryptedData: TFDQuery;
+    FDPhysMSSQLDriverLink: TFDPhysMSSQLDriverLink;
   private
   public
   end;
