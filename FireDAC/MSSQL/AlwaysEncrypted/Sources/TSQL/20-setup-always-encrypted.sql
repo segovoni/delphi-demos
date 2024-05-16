8a03e7fd2717af71748201a059ec354e5d754072
Sergio Govoni <segovoni@gmail.com>
2024-05-16 10:18:02 +0200

    Improved links to SQL Server encryption docs

--- FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/20-setup-always-encrypted.sql
+++ FireDAC/MSSQL/AlwaysEncrypted/Sources/TSQL/20-setup-always-encrypted.sql
@@ -13,26 +13,29 @@
 USE [master];
 GO
 
+-- SQL Server encryption
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/sql-server-encryption?WT.mc_id=DP-MVP-4029181
+
 -- Always Encrypted documentation
--- https://learn.microsoft.com/azure/azure-sql/database/always-encrypted-landing 
+-- https://learn.microsoft.com/azure/azure-sql/database/always-encrypted-landing?WT.mc_id=DP-MVP-4029181
 
 -- Analyze the impacts due to the possible change of COLLATE for the encrypted columns
--- https://learn.microsoft.com/sql/relational-databases/collations/collation-and-unicode-support
+-- https://learn.microsoft.com/sql/relational-databases/collations/collation-and-unicode-support?WT.mc_id=DP-MVP-4029181
 
 -- Analyze possible impacts on client applications (parametric queries, connection strings, supported drivers)
--- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-client-development
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-client-development?WT.mc_id=DP-MVP-4029181
 
 -- How queries against encrypted columns work
--- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-database-engine#how-queries-against-encrypted-columns-work
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-database-engine?WT.mc_id=DP-MVP-4029181#how-queries-against-encrypted-columns-work
 
 -- Analyze existing Limitations
--- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-database-engine#limitations
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/always-encrypted-database-engine?WT.mc_id=DP-MVP-4029181#limitations
 
 -- Configure Always Encrypted using SQL Server Management Studio
--- https://learn.microsoft.com/sql/relational-databases/security/encryption/configure-always-encrypted-using-sql-server-management-studio
+-- https://learn.microsoft.com/sql/relational-databases/security/encryption/configure-always-encrypted-using-sql-server-management-studio?WT.mc_id=DP-MVP-4029181
 
 -- Microsoft ODBC Driver for SQL Server on Windows
--- https://learn.microsoft.com/sql/connect/odbc/windows/microsoft-odbc-driver-for-sql-server-on-windows
+-- https://learn.microsoft.com/sql/connect/odbc/windows/microsoft-odbc-driver-for-sql-server-on-windows?WT.mc_id=DP-MVP-4029181
 
 -- Working with column master key stores
 -- https://learn.microsoft.com/sql/connect/odbc/using-always-encrypted-with-the-odbc-driver?WT.mc_id=DP-MVP-4029181#working-with-column-master-key-stores
